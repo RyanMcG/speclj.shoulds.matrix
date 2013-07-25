2f95b28acce5477b0434ac3f63752094730b8b04
Ryan McGowan <ryan@ryanmcg.com>
2013-07-24 22:30:11 -0700

    Add tests.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject speclj.shoulds.matrix "0.1.0"
+(defproject speclj.shoulds.matrix "0.1.1-SNAPSHOT"
   :description "Custom should(s) for clojure.core.matrix comparisons."
   :url "https://github.com/RyanMcG/speclj.shoulds.matrix"
   :license {:name "The MIT License"
@@ -6,6 +6,7 @@
             :distribution :repo
             :comments "Copyright 2013 Ryan McGowan"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
-  :profiles {:dev {:dependencies [[speclj "2.7.4"]]}}
+  :profiles {:dev {:dependencies [[net.mikera/vectorz-clj "0.10.0"]
+                                  [speclj "2.7.4"]]}}
   :plugins [[speclj "2.7.4"]]
   :test-paths ["spec"])
--- spec/speclj/shoulds/matrix/core_spec.clj
+++ spec/speclj/shoulds/matrix/core_spec.clj
@@ -1,5 +1,12 @@
 (ns speclj.shoulds.matrix.core-spec
-  (:require [speclj.core :refer :all]
+  (:require [clojure.core.matrix :refer [matrix]]
+            [speclj.core :refer :all]
             [speclj.shoulds.matrix.core :refer :all]))
 
-(describe "should-matrix==")
+(describe "should-matrix=="
+  (it "works with a call to matrix"
+    (should-matrix== [[1 2] [3 4]]
+                     (matrix [[1 2] [3 4]])))
+  (it "works without a call to matrix"
+    (should-matrix== [[1 2] [3 4]]
+                     [[1 2] [3 4]])))
